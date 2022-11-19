bd117e818dc32ce28db71cbca7cc6d3191ec1d66
szaraz levente <lalabacsi@gmail.com>
2022-11-19 17:49:39 +0200

    meg javit

--- 001szamitogepesarchitectura/feladatok02/fej11fel3/src/Main.java
+++ 001szamitogepesarchitectura/feladatok02/fej11fel3/src/Main.java
@@ -26,7 +26,7 @@ public class Main {
             if (file.equals("*")) {
                 break;
             }
-            Path ujFajlUtja = Paths.get(args[0], file + ".txt"); //arg[0] helyett ujFolderUtja
+            Path ujFajlUtja = Paths.get(ujFolderUtja.toString(), file); //arg[0] helyett ujFolderUtja
             Files.createFile(ujFajlUtja);
 
         }
