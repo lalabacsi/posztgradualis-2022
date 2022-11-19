063ae0209fc9a4170324d9da87a67863455159d1
szaraz levente <lalabacsi@gmail.com>
2022-11-19 13:04:33 +0200

    argumentum

--- 001szamitogepesarchitectura/feladatok02/fej11fel2/src/Main.java
+++ 001szamitogepesarchitectura/feladatok02/fej11fel2/src/Main.java
@@ -1,6 +1,26 @@
-public class Main {
-    public static void main(String[] args) {
+import java.awt.*;
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
-        System.out.println("Hello world!");
+public class Main {
+    public static void main(String[] args)
+    //Írja ki a program prancssori argumentumaként megkapott könyvtárban lévő összes könyvtár és
+    //fájl nevét. Egyszer a könyvtrárakat, utána a fájlokat.
+    {
+        Path currentFolderPath = Paths.get(args[0]).toAbsolutePath();
+        File currentFolderInfo = currentFolderPath.toFile();
+
+        File[] filerendszer = currentFolderInfo.listFiles();
+
+        for (int i = 0; i < filerendszer.length; i++) {
+            if (!filerendszer[i].isFile())
+                System.out.println("Konyvtar : " + filerendszer[i]);
+
+        }
+        for (int i = 0; i < filerendszer.length; i++) {
+            if (filerendszer[i].isFile())
+                System.out.println("File : " + filerendszer[i]);
+        }
     }
 }
