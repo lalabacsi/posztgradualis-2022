47839653347144e3659d3b4007cad26fb61048fd
szaraz levente <lalabacsi@gmail.com>
2022-11-19 12:36:03 +0200

    fdfd

--- 001szamitogepesarchitectura/feladatok02/fej11fel1/src/Main.java
+++ 001szamitogepesarchitectura/feladatok02/fej11fel1/src/Main.java
@@ -1,5 +1,28 @@
+import java.awt.*;
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    //1. Írja ki a program aktuális könyvtárában lévő összes könyvtár és fájl nevét.
+    // Egyszer a könyvtrárakat,
+    //utána a fájlokat.
+    //Path aktualisKonyvtar = Paths.get(".");
+    //Path aktualisKonyvtarAbsolute = aktualisKonyvtar.toAbsolutePath().normalize();
+    {
+      Path currentFolderPath = Paths.get("").toAbsolutePath();
+      File currentFolderInfo = currentFolderPath.toFile();
+
+      File[] filerendszer = currentFolderInfo.listFiles();
+
+      for(int i = 0; i<filerendszer.length; i++){
+          if (!filerendszer[i].isFile())
+          System.out.println("Konyvtar : " + filerendszer[i]);
+
+      }
+        for(int i = 0; i<filerendszer.length; i++) {
+            if (filerendszer[i].isFile())
+                System.out.println("File : " + filerendszer[i]);
+        }
     }
 }
