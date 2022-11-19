e44a7c28a3669188ad52b0d53aa2d4f9d81d6eeb
szaraz levente <lalabacsi@gmail.com>
2022-11-19 17:31:38 +0200

    jav

---
+++ 001szamitogepesarchitectura/feladatok02/fej11fel3/src/Main.java
@@ -0,0 +1,36 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        //Írjunk egy programot ami az aktuális könyvtárban létrehoz egy _DataFiles könyvtárat, majd a
+        //consoleról *-ig beolvasott nevű fájlokat hoz létre ebben a könyvtárban.
+        //(Próbáld ki, hogy mi történik, ha olyan fájlneveket adsz meg, amik nem kanonikus utak. Mit
+        //gondolsz, miért lehet ebből baj?)
+
+        //String fileName = "/home/janbodnar/docs/memos";
+        //        Path path = Paths.get(fileName);
+        //        Files.createDirectories(path);
+        Scanner sc = new Scanner(System.in);
+
+        Path ujFolderUtja = Paths.get("_DataFiles");
+        Files.createDirectories(ujFolderUtja);
+
+        do {
+            System.out.println("Adjon nevet a file-nak: ");
+            String file = sc.nextLine();
+            if (file.equals("*")) {
+                break;
+            }
+            Path ujFajlUtja = Paths.get(args[0], file + ".txt");
+            Files.createFile(ujFajlUtja);
+
+        }
+        while (true);
+
+    }
+}
