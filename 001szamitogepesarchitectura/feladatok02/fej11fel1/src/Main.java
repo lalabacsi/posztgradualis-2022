d2b06a93b0db88cd6696e055f6d921dbbeb3313a
szaraz levente <lalabacsi@gmail.com>
2022-11-19 12:51:07 +0200

    file konyvtar

--- 001szamitogepesarchitectura/feladatok02/fej11fel1/src/Main.java
+++ 001szamitogepesarchitectura/feladatok02/fej11fel1/src/Main.java
@@ -2,25 +2,24 @@ import java.awt.*;
 import java.io.File;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+
 public class Main {
     public static void main(String[] args)
     //1. Írja ki a program aktuális könyvtárában lévő összes könyvtár és fájl nevét.
     // Egyszer a könyvtrárakat,
     //utána a fájlokat.
-    //Path aktualisKonyvtar = Paths.get(".");
-    //Path aktualisKonyvtarAbsolute = aktualisKonyvtar.toAbsolutePath().normalize();
     {
-      Path currentFolderPath = Paths.get("").toAbsolutePath();
-      File currentFolderInfo = currentFolderPath.toFile();
+        Path currentFolderPath = Paths.get("").toAbsolutePath();
+        File currentFolderInfo = currentFolderPath.toFile();
 
-      File[] filerendszer = currentFolderInfo.listFiles();
+        File[] filerendszer = currentFolderInfo.listFiles();
 
-      for(int i = 0; i<filerendszer.length; i++){
-          if (!filerendszer[i].isFile())
-          System.out.println("Konyvtar : " + filerendszer[i]);
+        for (int i = 0; i < filerendszer.length; i++) {
+            if (!filerendszer[i].isFile())
+                System.out.println("Konyvtar : " + filerendszer[i]);
 
-      }
-        for(int i = 0; i<filerendszer.length; i++) {
+        }
+        for (int i = 0; i < filerendszer.length; i++) {
             if (filerendszer[i].isFile())
                 System.out.println("File : " + filerendszer[i]);
         }
